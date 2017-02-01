beabda964683b5c098fe6459a590e5f7ccf87229
Rajkaran164 <rajkara164@gmail.com>
2017-02-01 10:19:48 -0800

    firstprogram

---
+++ H.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	switch(4)
+	{
+		default:
+		printf("Hello");
+		case 1:
+		printf("Raj");
+		case 2:
+		printf("\nKaran");
+
+	}
+	getch();
+}
