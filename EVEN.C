9d7bde66ae7477d555e4f817824ee2f780f9990e
Rajkaran164 <rajkara164@gmail.com>
2017-02-03 08:28:09 -0800

    3/02/2017

---
+++ EVEN.C
@@ -0,0 +1,13 @@
+// Program to check the number is even or odd without using if- else
+#include <stdio.h>
+int main()
+{
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    (number % 2 == 0) ? printf("%d is even.", number) : printf("%d is odd.", number);
+
+    return 0;
+}
